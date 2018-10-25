b088e973406aa66419bf57dbaeb141b26412c139
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-25 14:37:14 -0500

    got API request to return results

--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -3,17 +3,17 @@ const restaurantBtn = document.querySelector(".search--restaurants")
 
 //  defining a function to get the data from Zomato API and turn it into JavaScript
 const getRestaurantData = () => {
-  return fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&q=italian", {
-  headers: {
-    "Content-Type": "application/json",
-    "user-key": "335446a1c8d4b48be8722101b589b84d"
-  }
-})
-  .then(data => data.json)
-  .then(returns=> console.log(returns))
+  return fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=Italian", {
+    headers: {
+      "user-key": "335446a1c8d4b48be8722101b589b84d"
+    }
+  })
+    .then(data => data.json())
+    .then(returns => { console.log(returns)})
 }
 
 // add event listener to search button that fetches data when clicked
-restaurantBtn.addEventListener("click", ()=> {
- getRestaurantData()
+restaurantBtn.addEventListener("click", () => {
+  getRestaurantData()
+
 })
