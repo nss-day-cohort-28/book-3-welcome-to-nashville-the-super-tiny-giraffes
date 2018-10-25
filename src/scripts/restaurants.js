b8f7979181fb2b54be52b25df937a1492efc6d85
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-25 15:22:30 -0500

    committed before merging with master

--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -1,10 +1,8 @@
 // obtain reference to restaurant search button
+let restCounter = 0
 const restaurantBtn = document.querySelector(".search--restaurants")
 
 //  defining a function to get the data from Zomato API and turn it into JavaScript
-
-
-
 const getRestaurantData = () => {
   return fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=Italian", {
     headers: {
@@ -12,11 +10,25 @@ const getRestaurantData = () => {
     }
   })
     .then(data => data.json())
-    .then(returns => { console.log(returns.restaurants[1].restaurant.name) })
 }
 
-// add event listener to search button that fetches data when clicked
-restaurantBtn.addEventListener("click", () => {
-  getRestaurantData()
+// defining a function to loop over data object and grab names of restaurants
+const restaurantItemMaker = (restaurantObject) => {
+  restaurantObject.restaurants.forEach(currentRestaurant => {
+    counter ++
+    let currentRestName = currentRestaurant.restaurant.name
+    let currentRestLocation = currentRestaurant.restaurant.location.address
+    let restItem = `${counter}. ${currentRestName}: ${currentRestLocation}`
+    return restItem
+  })
+}
 
+
+// add event listener to search button that fetches data when clicked
+restaurantBtn.addEventListener("click", (event) => {
+  let restaurantData = getRestaurantData(event)
+  
 })
+
+
+
