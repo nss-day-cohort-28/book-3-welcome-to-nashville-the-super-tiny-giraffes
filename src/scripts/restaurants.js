5688dc7ef8a2dc2a52f540d8d2cc5378e2e3d1b1
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-25 15:36:29 -0500

    commiting before merging master

--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -1,5 +1,4 @@
 // obtain reference to restaurant search button
-let restCounter = 0
 const restaurantBtn = document.querySelector(".search--restaurants")
 
 //  defining a function to get the data from Zomato API and turn it into JavaScript
@@ -25,8 +24,15 @@ const restaurantItemMaker = (restaurantObject) => {
 
 
 // add event listener to search button that fetches data when clicked
-restaurantBtn.addEventListener("click", (event) => {
-  let restaurantData = getRestaurantData(event)
+restaurantBtn.addEventListener("click", (event) => { 
+    return fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=Italian", {
+    headers: {
+    "user-key": "335446a1c8d4b48be8722101b589b84d"
+    }
+  })
+  .then(data => data.json())
+  .then()
+
   
 })
 
