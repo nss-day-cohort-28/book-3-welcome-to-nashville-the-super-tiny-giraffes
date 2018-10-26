51115da96003ebe3253fcbd0af2af76894dcaca9
Brendan McCray <rbmccr@gmail.com>
2018-10-26 14:37:22 -0500

    removed unecessary javascript function and fetch. Added some comments

--- src/scripts/saveditinerary.js
+++ src/scripts/saveditinerary.js
@@ -4,8 +4,12 @@ function saveItinerary() {
   fetch("http://localhost:8088/itinerary/1")
     .then(jsonData => jsonData.json())
     .then(data => {
+      if (data.park === "" && data.restaurant === "" && data.meetup === "" && data.concert === "") {
+        return
+      } else {
       savedItinerary.innerHTML += `
       <br /> Park: ${data.park} <br /> Restaurant: ${data.restaurant} <br /> Meetup: ${data.meetup} <br /> Concert: ${data.concert} <br /> `;
       clearItinerary();
+      }
     })
 }
