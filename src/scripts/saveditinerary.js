2b2c44155efa518bf48d523fc0cd376c5b56885e
Brendan McCray <rbmccr@gmail.com>
2018-10-26 15:46:44 -0500

    added functionality to create (post) new objects in itinerary database when save itinerary button is clicked

--- src/scripts/saveditinerary.js
+++ src/scripts/saveditinerary.js
@@ -1,19 +1,38 @@
 let savedItinerary = document.getElementById("itinerary--saved");
+let savedItineraryObject = {};
 
 function saveItinerary() {
-  fetch("http://localhost:8088/itinerary/1")
+    fetch("http://localhost:8088/itinerary/1")
     .then(jsonData => jsonData.json())
     .then(data => {
       if (data.park === "" && data.restaurant === "" && data.meetup === "" && data.concert === "") {
         return
-      } else if (savedItinerary.innerHTML === "") {
-        savedItinerary.innerHTML += `
-        Park: ${data.park} <br /> Restaurant: ${data.restaurant} <br /> Meetup: ${data.meetup} <br /> Concert: ${data.concert} <br /> `;
-        clearItinerary();
       } else {
-        savedItinerary.innerHTML += `
-        <br /> Park: ${data.park} <br /> Restaurant: ${data.restaurant} <br /> Meetup: ${data.meetup} <br /> Concert: ${data.concert} <br /> `;
-        clearItinerary();
+      savedItineraryObject = {
+        park: data.park,
+        restaurant: data.restaurant,
+        meetup: data.meetup,
+        concert: data.concert
+      }
+      fetch("http://localhost:8088/itinerary", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(savedItineraryObject)
+      })
+        .then(jsonData => jsonData.json())
+        .then(data => {
+          if (savedItinerary.innerHTML === "") {
+            savedItinerary.innerHTML += `
+            Park: ${data.park} <br /> Restaurant: ${data.restaurant} <br /> Meetup: ${data.meetup} <br /> Concert: ${data.concert} <br /> `;
+            clearItinerary();
+          } else {
+            savedItinerary.innerHTML += `
+            <br /> Park: ${data.park} <br /> Restaurant: ${data.restaurant} <br /> Meetup: ${data.meetup} <br /> Concert: ${data.concert} <br /> `;
+            clearItinerary();
+          }
+        })
       }
     })
-}
+  }
